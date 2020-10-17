beaec0abdcb5c939a72be4b8e0a64c61d5fa32a6
omkant rajbanshi <lopsillent@gmail.com>
2020-10-17 19:02:29 +0545

    Vesion 1.0.3

--- client/src/store.js
+++ client/src/store.js
@@ -1,24 +1,27 @@
-// import { createStore, applyMiddleware, compose } from 'redux';
-// import thunk from 'redux-thunk';
-// import rootreducer from './reducers'
-
-// const initialState = {};
-
-// const middleware = [thunk];
-
-// const store = createStore (rootreducer, initialState, compose(
-//     applyMiddleware(...middleware),
-//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-//     )
-// );
-
-// export default store;
-
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+
+import thunk from 'redux-thunk';
+import rootreducer from './reducers'
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore (rootreducer, initialState, composeWithDevTools(
+    applyMiddleware(...middleware),
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+);
+
+export default store;
+
+// import { createStore, applyMiddleware } from 'redux';
+// import { composeWithDevTools } from 'redux-devtools-extension';
  
-const store = createStore(reducer, composeWithDevTools(
-  applyMiddleware(...middleware),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  // other store enhancers if any
-));
+// const store = createStore(reducer, composeWithDevTools(
+//   applyMiddleware(...middleware),
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   // other store enhancers if any
+// ));
+// export default store;
